e90b19d329bacda44646443ba4d64b65877a31c7
vascobarreiros <vasco.barreiros@wndgroup.io>
2021-01-08 08:06:36 +0000

    Initial Commit

---
+++ GeoLoka/GeoLokaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoLokaApp.swift
+//  GeoLoka
+//
+//  Created by Vasco Barreiros on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GeoLokaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
